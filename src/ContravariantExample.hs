2965836a71871071e3b50a23288183ed94acac59
Ivan Murashko <ivan.murashko@gmail.com>
2018-11-09 15:46:36 +0300

    Contravariant example added

--- src/ContravariantExample.hs
+++ src/ContravariantExample.hs
@@ -4,11 +4,11 @@ import Data.Functor.Contravariant hiding (Predicate)
 
 newtype Predicate a = Predicate { runPredicate :: a -> Bool}
 
-instance Contravariant Predicate where
-    contramap f (Predicate p) = Predicate ( p . f )
-
 intgt10 :: Predicate Int
 intgt10 = Predicate ( \i -> i > 10 )
+
+instance Contravariant Predicate where
+    contramap f (Predicate p) = Predicate ( p . f )
 
 strgt10 :: Predicate [Char]
 strgt10 = contramap length intgt10
